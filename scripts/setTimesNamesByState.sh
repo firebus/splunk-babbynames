579e6fa7be58ebb8bc834d4844f69ea072cb6e43
firebus <russ@firebus.com>
2013-06-09 07:59:34 -0700

    version 0.1

---
+++ scripts/setTimesNamesByState.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# touch $year $month $day $filename
+function updateFileModTime {
+    touch -d $1-$2-$3 $4
+}
+
+DAY=1
+MONTH=1
+YEAR=2001
+FILES=`ls *.TXT`
+for file in $FILES
+do
+    updateFileModTime $YEAR $MONTH $DAY $file
+    if [ "$?" -ne "0" ]; then
+	MONTH=`expr $MONTH + 1`
+        DAY=1
+	updateFileModTime $YEAR $MONTH $DAY $file
+    fi
+    DAY=`expr $DAY + 1`
+done
